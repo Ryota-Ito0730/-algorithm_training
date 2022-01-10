cc09c6c5a808d27290fb0fbb709a341f94b1b45d
Ryota-Ito0730 <rgitmain01@gmail.com>
2022-01-10 11:58:59 +0900

    実験的にローカルリポジトリ名を変更_algorithm_trainingのaを2つ追加

--- hello-world/test.py
+++ hello-world/test.py
@@ -1,1 +1,18 @@
-print('hello wprld')
+# 二次元配列mを作る
+
+s_lists = [1, 2, 3, 4, 5, 6]
+u_lists = [1, 2, 3, 4, 5]
+m_dictional = {}
+
+i = 0
+k = 0
+for s_value in s_lists:
+    m_dictional[s_value] = s_value
+    i += 1
+
+i = 0
+for u_value in u_lists:
+    m_dictional[i] = u_value
+    i += 1
+
+print(m_dictional)
