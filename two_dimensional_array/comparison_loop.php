48702b23eb3ed3949b94592704b9f56149bb07ca
Ryota-Ito0730 <rgitmain01@gmail.com>
2022-01-19 08:31:55 +0900

    比較ループ再トライ_2

--- two_dimensional_array/comparison_loop.php
+++ two_dimensional_array/comparison_loop.php
@@ -5,21 +5,28 @@ $array_t = [1 => 23, 2 => 35, 3 => 18, 4 => 50];
 $max = count($array_t);
 $array_q = [];
 // 比較した結果の降順の順位番号を入れる。初期値は1で埋める
-$i = 0;
-for ($i; $i < $max; $i++) {
-  $array_q[$i + 1] = 1;
+$i = 1;
+for ($i; $i <= $max; $i++) {
+  $array_q[$i] = 1;
 }
 var_dump($array_q);
 
 $j = 1;
+$l = 1;
+$tk = "";
+$tl = "";
 for ($j; $j <= $max - 1; $j++) {
-  $tk =  $array_t[$j];
-  $tl =  $array_t[$j + 1];
-  if ($tk < $tl) {
-    $array_q[$j] = $array_q[$j] + 1;
-  } elseif ($tk > $tl) {
-    $array_q[$j + 1] = $array_q[$j + 1] + 1;
+  $tk = $array_t[$j];
+  for ($l; $l <= $max; $l++) {
+    $tl = $array_t[$l];
+    var_dump($tl);
+    if ($tk < $tl) {
+      $array_q[$j] = $array_q[$j] + 1;
+    } elseif ($tk > $tl) {
+      $array_q[$j + 1] = $array_q[$j + 1] + 1;
+    }
   }
+  $l = 1;
 }
 
 echo '<pre>';
