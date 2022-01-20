c768bcac8f868c0b94c9f9ef7263176641f02f36
Ryota-Ito0730 <rgitmain01@gmail.com>
2022-01-20 20:59:44 +0900

    P74_P80_実践問題2をトライ_1

---
+++ two_dimensional_array/P74_P80_fares/fares.php
@@ -0,0 +1,56 @@
+<?php
+include("./station_name_sign.php");
+// 駅リストを元に料金表を作成する
+$max = count($array_station_name_sign);
+$fares_y = [];
+$fares_x = [];
+$i = 1;
+$j = 1;
+for ($i; $i <= $max; $i++) {
+  $fares_y[$i] = $i;
+  for ($j; $j <= $max; $j++) {
+    $fares_x[$j] = $j;
+  }
+  $fares_y[$i] = $fares_x;
+  $j = 1;
+}
+$k = 1;
+// 中央線部分を0で埋める
+for ($k; $k <= $max; $k++) {
+  $fares_y[$k][$k] = 0;
+};
+
+// 中央線右半分の料金を埋める(10えんずつあげる)
+$l = 1;
+$m = 2;
+for ($l; $l <= $max - 1; $l++) {
+  for ($m; $m <= $max; $m++) {
+    $fares_y[$l][$m] = 10 * ($m - 1);
+  }
+  $m = 2 + $l;
+};
+
+// 中央線左半分の料金を埋める(10えんずつあげる)
+$l = 2;
+$m = 1;
+for ($l; $l <= $max; $l++) {
+  for ($m; $m <= ($max - $m); $m++) {
+    $fares_y[$l][$m] = 10 * $m;
+  }
+  $m = 1 + ($l - 1);
+};
+
+echo '↓配列$fares_y(料金表)の処理結果' . '<br>';
+$y = 1;
+$z = 1;
+for ($y; $y <= $max; $y++) {
+  // $fares_y[$y];
+  for ($z; $z <= $max; $z++) {
+    echo $fares_y[$y][$z] . ',';
+  }
+  echo '<br>';
+  $z = 1;
+}
+// echo "<pre>";
+// var_dump();
+// echo "</pre>";
