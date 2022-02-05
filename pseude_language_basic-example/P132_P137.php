708441e4f1f6a8d90a12e50170354d933c0e0d63
Ryota-Ito0730 <rgitmain01@gmail.com>
2022-02-05 19:24:43 +0900

    文字のカウント_20220205_1924

--- pseude_language_basic-example/P132_P137.php
+++ pseude_language_basic-example/P132_P137.php
@@ -1,36 +1,8 @@
 <?php
 // P132_P137:文字のカウント
-$alphabet_array = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+$alphabet_array = ['a', 'b', 'c', 'd', 'e', 'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z'];
 // 文字配列(#が選択された時点で処理は終了を示す)
 $moji_array = [1 => 'g', 2 => 'i', 3 => 'j', 4 => 'i', 5 => 'g', 6 => 'e', 7 => 'n', 8 => 'g', 9 => 'o', 10 => '#'];
-
 $max = count($alphabet_array); // 26
 $count_array = [];
 $i = 1;
@@ -39,7 +11,6 @@ $j = 1;
 for ($j; $j <= $max; $j++) {
   $count_array[$j] = 0;
 }
-
 $i = 1;
 while ($moji_array[$i] !== '#') {
   $j = 1;
@@ -51,10 +22,14 @@ while ($moji_array[$i] !== '#') {
   $count_array[$j] = $count_array[$j] + 1;
   $i++;
 }
-
-$count_result = array_combine($alphabet_array,  $count_array);
-
-echo '<pre>';
-// var_dump($count_array);
-var_dump($count_result);
-echo '</pre>';
+$j = 1;
+for ($j; $j <= $max; $j++) {
+  if ($count_array[$j] !== 0) {
+    echo '文字' . $alphabet_array[$j - 1] . '  ' . $count_array[$j] . '個<br>';
+  }
+} 
+// $count_result = array_combine($alphabet_array,  $count_array);
+// echo '<pre>';
+// // var_dump($count_array);
+// var_dump($count_result);
+// echo '</pre>';
